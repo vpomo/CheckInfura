90ab9583f500656a59bce369e49b019b484e01d9
vpomo <pomogalov@gmail.com>
2018-08-12 23:26:42 +0300

    add postgre.js

--- nodejs/postgre.js
+++ nodejs/postgre.js
@@ -1,7 +1,15 @@
 //https://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example#simple-select
 //https://rage.mp/forums/topic/202-%D1%80%D0%B0%D0%B1%D0%BE%D1%82%D0%B0%D0%B5%D0%BC-%D1%81-postgresql/
-var pgp = require("pg-promise")(/*options*/);                 
-var db = pgp("postgres://token:**@127.0.0.1:5439/tokendb");
+var pgp = require("pg-promise")(/*options*/);
+const cn = {
+    host: 'localhost',
+    port: 5439,
+    database: 'tokendb',
+    user: 'token',
+    password: '**'
+};
+var db = pgp(cn);
+//var db = pgp("postgres://token:**@127.0.0.1:5439/tokendb");
 
 db.any("INSERT INTO users(name, age) VALUES($1, $2)", ["vasya", 33])
     .then(data => {
@@ -11,7 +19,7 @@ db.any("INSERT INTO users(name, age) VALUES($1, $2)", ["vasya", 33])
     console.log('ERROR:', error); // print error;
 });
 
-/*
+
 db.any("INSERT INTO users(name, age) VALUES($1, $2)", ["John", 21])
     .then(function (data) {
         console.log("DATA:", data.value);
@@ -19,7 +27,7 @@ db.any("INSERT INTO users(name, age) VALUES($1, $2)", ["John", 21])
     .catch(error => {
     console.log("ERROR:", error);
     });
-*/
+
 
 db.any("select * from users where name = $1", ["vasya"])
     .then(function (data) {
@@ -29,14 +37,16 @@ db.any("select * from users where name = $1", ["vasya"])
         console.log("ERROR:", error);
     });
 
+/*
 db.result("DELETE FROM users WHERE name = $1", ["John"])
     .then(result => {
     // rowCount = number of rows affected by the query
     console.log(result.rowCount); // print how many records were deleted;
-})
-.catch(error => {
+    })
+    .catch(error => {
     console.log('ERROR:', error);
 });
+*/
 
 
 /*
