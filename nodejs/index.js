3ea041df732a732f1240b75f2741bd6fce88f52f
vpomo <pomogalov@gmail.com>
2018-08-12 19:19:42 +0300

    add infura.js

--- nodejs/index.js
+++ nodejs/index.js
@@ -4,4 +4,4 @@ var fp = require("./asyncParser");
 var fromFile = fp.readCsvFile("myfile.csv");
 console.log(fp.result);
 console.log(fp.rr());
-//server.startHttpServer();
+server.startHttpServer();
