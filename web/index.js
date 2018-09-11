1e00f2232ef8ec10f1b10a4291d1be13d33e2d20
vpomo <pomogalov@gmail.com>
2018-09-12 00:05:17 +0300

    add new request

--- web/index.js
+++ web/index.js
@@ -8,32 +8,71 @@ $(document).ready(function () {
     getAddressWallet();
 });
 
-function createWallet(callbackfunction) {
-        var url = "http://localhost:8080/api/wallet/";
-        var args = {"idWordpress": idWordpress};
-        $.post(url, args, function (data) {
-			getAddressWallet();
-            console.log(data);
-            var goodsSpecs = data.goodsSpecs;
-            if (callbackfunction != undefined) {
-                callbackfunction();
-            }
-        });
-}
-
 function getAddressWallet(){
 	$.ajax({
 		  url: "http://localhost:8080/api/wallet",
-		  type: "get", //send it through get method
+		  type: "get", 
 		  data: { 
 				idWordpress: idWordpress
 		  },
 		  success: function(response) {
-				$('#addressWallet').html(response);
-				return response;
+				$('#addressWallet').html(response.addressWallet);
+				return response.addressWallet;
 		  },
 		  error: function(xhr) {
 				console.log("GET Not request - http://localhost:8080/api/wallet");
+		  }
+	});
+}
+
+function getTaskHistory(){
+	$.ajax({
+		  url: "http://localhost:8080/api/task/all/" + idWordpress,
+		  type: "get", 
+		  success: function(response) {
+			  console.log(response);
+				return response;
+		  },
+		  error: function(xhr) {
+				console.log("GET Not request - http://localhost:8080/api/task/history/");
+		  }
+	});
+}
+
+function createWallet() {
+	var sendData = { 
+				idWordpress: idWordpress
+		  };
+	$.ajax({
+		url : "http://localhost:8080/api/wallet/",
+		type : "POST",
+		data: JSON.stringify(sendData),
+        contentType: "application/json; charset=utf-8",
+		traditional: true,
+		success: function(response) {
+			  console.log("Success");
+		  },
+		error: function(xhr) {
+			  console.log("Error");
+		  }
+	});
+}
+
+function createTask() {
+	var sendData = { 
+				idWordpress: idWordpress
+		  };
+	$.ajax({
+		url : "http://localhost:8080/api/task/",
+		type : "POST",
+		data: JSON.stringify(sendData),
+        contentType: "application/json; charset=utf-8",
+		traditional: true,
+		success: function(response) {
+			  console.log("Success");
+		  },
+		error: function(xhr) {
+			  console.log("Error");
 		  }
 	});
 }
